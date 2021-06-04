071ef95e55eb2f90d1b6fbe5cb26fc3563269ff2
Metiarizmus <72821850+Metiarizmus@users.noreply.github.com>
2021-06-04 20:15:21 +0300

    second try

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,19 @@ import java.io.IOException;
 
 public class Main {
 
+    private static final String ONE = "https://jsonplaceholder.typicode.com/posts";
+    private static final String TWO = "https://jsonplaceholder.typicode.com/comments";
+    private static final String THREE = "https://jsonplaceholder.typicode.com/photos";
+    private static final String FOUR = "https://jsonplaceholder.typicode.com/todos";
+    private static final String FIVE = "https://jsonplaceholder.typicode.com/users";
+    private static final String SIX = "https://jsonplaceholder.typicode.com/posts/1/comments";
+
     public static void main(String[] args) throws IOException {
 
         GetJsonHttp getJsonHttp = new GetJsonHttp();
 
 
-        getJsonHttp.provide("https://jsonplaceholder.typicode.com/users");
+        getJsonHttp.provide(FIVE);
 
     }
 }
